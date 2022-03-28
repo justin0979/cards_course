129be3f9280001cb939d587555e089d035764c46
justin0979 <justinm0979@gmail.com>
2022-03-28 13:30:21 -0500

    add jpegs and finish card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this awesome tree during my hike today.',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'hikingperson',
+      imageUrl: 'assets/mountain.jpeg',
+      content:
+        'Beautiful view of some mountains I saw during my hike.',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking12222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'Action shot of me riding my bike.',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking12222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'Action shot of me riding my bike.',
+    },
+  ];
 }
